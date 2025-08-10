db727e19652095cb82a8da1281b7e8cbd717d659
RhysRoo <rhys.bundy97@gmail.com>
2025-08-10 22:42:22 +0100

    setup base project

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 
 function Homepage() {
     return(
-        <div className="flex flex-col bg-black space-y-8 p-8">
-            <div className="flex flex-row bg-white space-x-8 p-8">
-                <h1 className="text-lg text-red-500">Hello react in elect!!</h1>
-                <h1 className="text-lg text-red-500">Homepage</h1>
-            </div>
-            <div className="flex flex-row bg-white space-x-8 p-8">
-                <h1 className="text-lg text-red-500">Hello react in electron</h1>
-                <h1 className="text-lg text-red-500">Homepage</h1>
-            </div>
+        <div>
+            
         </div>
     )
 }
